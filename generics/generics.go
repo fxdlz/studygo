4ba971bb05d16c890721bdeff28c9fbd5220867a
fxdlz <1970475803@qq.com>
2023-11-20 21:49:45 +0800

    slice delete func

--- generics/generics.go
+++ generics/generics.go
@@ -1,12 +1,13 @@
 package generics
 
+// Delete 删除对应下标索引的元素
 func Delete[T any](idx int, slice []T) []T {
 	if idx >= len(slice) || idx < 0 {
 		panic("idx out of range slice")
 	}
 	var newSlice []T
-	if len(slice)-1 < cap(slice)/2 {
-		newSlice = make([]T, cap(slice)/2)
+	if len(slice)-1 < (cap(slice)+1)/2 {
+		newSlice = make([]T, len(slice)-1, (cap(slice)+1)/2)
 		for i, j := 0, 0; i < len(slice); i++ {
 			if i == idx {
 				continue
--- test/slice_test.go
+++ test/slice_test.go
@@ -7,11 +7,20 @@ import (
 )
 
 func TestSliceDelete(t *testing.T) {
-	var slice = []int{1, 2, 3, 4, 5}
-	slice = generics.Delete(1, slice)
-	fmt.Printf("val:%v,len:%v,cap:%v\n", slice, len(slice), cap(slice))
-	slice = generics.Delete(1, slice)
-	fmt.Printf("val:%v,len:%v,cap:%v\n", slice, len(slice), cap(slice))
-	slice = generics.Delete(1, slice)
-	fmt.Printf("val:%v,len:%v,cap:%v\n", slice, len(slice), cap(slice))
+	var sliceInt = []int{1, 2, 3, 4, 5}
+	sliceInt = generics.Delete(1, sliceInt)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceInt, len(sliceInt), cap(sliceInt))
+	sliceInt = generics.Delete(1, sliceInt)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceInt, len(sliceInt), cap(sliceInt))
+	sliceInt = generics.Delete(1, sliceInt)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceInt, len(sliceInt), cap(sliceInt))
+
+	var sliceStr = []string{"1", "2", "3", "4", "5"}
+	sliceStr = generics.Delete(1, sliceStr)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceStr, len(sliceStr), cap(sliceStr))
+	sliceStr = generics.Delete(1, sliceStr)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceStr, len(sliceStr), cap(sliceStr))
+	sliceStr = generics.Delete(1, sliceStr)
+	fmt.Printf("val:%v,len:%v,cap:%v\n", sliceStr, len(sliceStr), cap(sliceStr))
+
 }
